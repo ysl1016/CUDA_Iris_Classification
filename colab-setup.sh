685055470b7dcd11b2ccf766efda258f2f5548c6
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-02-06 19:58:05 +0900

    Update colab-setup.sh

--- colab-setup.sh
+++ colab-setup.sh
@@ -5,10 +5,15 @@ export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 apt-get update
-apt-get install -y cmake build-essential
+apt-get install -y cmake build-essential libgtest-dev python3-pip
+
+cd /usr/src/gtest
+cmake CMakeLists.txt
+make
+cp lib/*.a /usr/lib
 
 pip install numpy pandas matplotlib seaborn
 
-mkdir -p {bin,lib,build,data,results}
+mkdir -p bin lib build data results
 
 chmod +x install.sh run.sh
