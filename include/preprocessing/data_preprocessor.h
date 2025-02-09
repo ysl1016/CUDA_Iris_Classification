b4c4f338f606cf0af06b79168a11180b96263487
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-02-09 17:53:15 +0900

    Update data_preprocessor.h

--- include/preprocessing/data_preprocessor.h
+++ include/preprocessing/data_preprocessor.h
@@ -5,19 +5,18 @@
 class DataPreprocessor {
 public:
     // Basic preprocessing methods
-    void normalizeFeatures(IrisData& data);
-    void standardizeFeatures(IrisData& data);
-    void splitData(const IrisData& data, IrisData& train_data, 
-                  IrisData& test_data, float test_ratio);
+    static void normalizeFeatures(IrisData& data);
+    static void standardizeFeatures(IrisData& data);
+    static void splitData(const IrisData& data, IrisData& train, IrisData& test, float train_ratio);
     
     // Advanced preprocessing methods
     void shuffleData(IrisData& data);
     void augmentData(IrisData& data, float noise_std = 0.1f);
     
+    static void calculateMeanAndStd(const float* data, int n_samples, int n_features, float* mean, float* std);
+    
 private:
     // Helper methods
-    void calculateMeanAndStd(const float* features, int n_samples, 
-                            int n_features, float* mean, float* std);
     void addGaussianNoise(float* features, int n_samples, 
                          int n_features, float std_dev);
 };
