7d8c71243d7ea6c6cf9f4af91acf6a08000ed6a3
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-02-09 18:15:03 +0900

    Update iris_data_loader.cu

--- src/data/iris_data_loader.cu
+++ src/data/iris_data_loader.cu
@@ -14,44 +14,44 @@ IrisDataLoader::~IrisDataLoader() {
     freeMemory();
 }
 
-bool IrisDataLoader::loadData(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << filename << std::endl;
-        return false;
-    }
-
+bool IrisDataLoader::loadData(IrisData& data) {
     std::vector<float> features;
     std::vector<int> labels;
-    std::string line;
-
-    // Read data from CSV file
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        
-        // Read features
-        for (int i = 0; i < 4; i++) {
-            std::getline(ss, value, ',');
-            features.push_back(std::stof(value));
-        }
-        
-        // Read label
-        std::getline(ss, value, ',');
-        labels.push_back(std::stoi(value));
+    
+    if (!loadFromFile(features, labels)) {
+        return false;
     }
-
+    
     data.n_samples = labels.size();
-    allocateMemory(data.n_samples);
-
-    // Copy data to GPU
-    CUDA_CHECK(cudaMemcpy(data.features, features.data(), 
-                         features.size() * sizeof(float), 
-                         cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(data.labels, labels.data(), 
-                         labels.size() * sizeof(int), 
-                         cudaMemcpyHostToDevice));
-
+    data.n_features = 4;
+    data.n_classes = 3;
+    
+    // Allocate device memory
+    if (cudaMalloc(&data.features, features.size() * sizeof(float)) != cudaSuccess) {
+        return false;
+    }
+    if (cudaMalloc(&data.labels, labels.size() * sizeof(int)) != cudaSuccess) {
+        cudaFree(data.features);
+        return false;
+    }
+    
+    // Copy data to device
+    if (cudaMemcpy(data.features, features.data(), 
+                   features.size() * sizeof(float), 
+                   cudaMemcpyHostToDevice) != cudaSuccess) {
+        cudaFree(data.features);
+        cudaFree(data.labels);
+        return false;
+    }
+    
+    if (cudaMemcpy(data.labels, labels.data(), 
+                   labels.size() * sizeof(int), 
+                   cudaMemcpyHostToDevice) != cudaSuccess) {
+        cudaFree(data.features);
+        cudaFree(data.labels);
+        return false;
+    }
+    
     return true;
 }
 
