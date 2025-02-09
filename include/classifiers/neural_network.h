21a8cf9d3d2acc4e4a63d078c46a14b725347b26
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-02-09 18:05:16 +0900

    Update neural_network.h

--- include/classifiers/neural_network.h
+++ include/classifiers/neural_network.h
@@ -5,26 +5,21 @@ class NeuralNetwork {
 public:
     NeuralNetwork(int input_size, int hidden_size, int output_size);
     ~NeuralNetwork();
-
-    void train(const IrisData& data, int epochs);
+    
+    void train(const float* features, const int* labels, int n_samples, 
+               float learning_rate = 0.01f, int epochs = 100);
     void predict(const float* features, int* predictions, int n_samples);
     float getAccuracy(const float* features, const int* labels, int n_samples);
-    void initializeParameters();
 
 private:
-    // Network parameters
-    float *d_W1, *d_W2;  // Weights
-    float *d_b1, *d_b2;  // Biases
-    float *d_h;          // Hidden layer activations
-    float *d_output;     // Output layer activations
+    void initializeParameters();
+    void forwardPass(const float* features, int n_samples);
     
     int input_size;
     int hidden_size;
     int output_size;
     
-    float* d_weights;
-    float learning_rate = 0.01f;
-    
-    void forwardPass(const float* features, int n_samples);
-    void backwardPass(const float* features, const int* labels, int n_samples);
+    float *d_W1, *d_W2;
+    float *d_b1, *d_b2;
+    float *d_h, *d_output;
 };
