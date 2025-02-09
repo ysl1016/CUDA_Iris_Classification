45f37ea70540a5354c4008aa4d6b3bfdd3ae2215
ysl1016 <154742313+ysl1016@users.noreply.github.com>
2025-02-09 18:56:26 +0900

    Update data_preprocessor.h

--- include/preprocessing/data_preprocessor.h
+++ include/preprocessing/data_preprocessor.h
@@ -3,10 +3,24 @@
 
 class DataPreprocessor {
 public:
+    static void normalizeFeatures(IrisData& data);
     static void standardizeFeatures(IrisData& data);
+    static void shuffleData(IrisData& data);
+    static void augmentData(IrisData& data, float noise_std = 0.1f);
     static void splitData(const IrisData& data, IrisData& train, IrisData& test, float train_ratio);
     
 private:
     static void calculateMeanAndStd(const float* data, int n_samples, int n_features,
                                   float* mean, float* std);
+    
+    struct VarianceOp {
+        float mean;
+        VarianceOp(float m) : mean(m) {}
+        
+        __host__ __device__
+        float operator()(float x) const {
+            float diff = x - mean;
+            return diff * diff;
+        }
+    };
 };
